34c6785ec04f677329957d2cb1976251eab02b72
djonymalta <djonymalta@Mac-mini-de-Djony.local>
2023-04-25 17:02:41 -0300

    Ajustes no nome dos métodos em 'displayName'

--- src/test/java/SimulacaoTest.java
+++ src/test/java/SimulacaoTest.java
@@ -2,6 +2,7 @@ import br.com.getnet.entity.ClientHttp;
 import br.com.getnet.entity.Pessoa;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,15 +27,17 @@ public class SimulacaoTest {
 
     // SIMULAÇÕES
     @Test
-    @DisplayName("GET: Valida status code de uma simulação com dados requeridos.")
+    @DisplayName("POST: Valida status code de uma simulação com dados requeridos.")
     public void validaSimulacaoAoCriarUmNovoUsuario() {
 
-        Set<ConstraintViolation<Pessoa>> violation = validator.validate(pessoa);
-        RestAssured.given().contentType(ContentType.JSON).body(violation).when().post(baseUrl.urlAPICreateUser()).then().log().all().statusCode(201);
+
+        RestAssured.given().contentType(ContentType.JSON).body(pessoa).when().post(baseUrl.urlAPICreateUser()).then().log().all().statusCode(201);
     }
 
+
+
     @Test
-    @DisplayName("GET: Valida campos obrigatórios.")
+    @DisplayName("POST: Valida campos obrigatórios.")
     public void validaSimulacaoComCamposObrigatorios() {
 
         Set<ConstraintViolation<Pessoa>> violation = validator.validate(pessoa);
